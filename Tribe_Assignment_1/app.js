c643b41d69ece19d13b75ec84e71bc47123f5b8c
kalpana123-1 <kalpanathamatam1822000@gmail.com>
2022-02-28 11:51:17 +0530

    update

--- Tribe_Assignment_1/app.js
+++ Tribe_Assignment_1/app.js
@@ -2,6 +2,7 @@ fetch('https://www.gov.uk/bank-holidays.json')
 
 .then(function(res){
     res.json().then(function(res){
+        console.log(res['england-and-wales'].events['0'].date);
         console.log(res);
     })
 })
